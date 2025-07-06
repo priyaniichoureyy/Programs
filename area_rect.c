be34fa7c7a13de528c84f0361ed58b5f3a1c74fb
Priyani Chourey <priyanichourey.dev@gmail.com>
2025-07-06 19:17:35 +0530

    Update area_rect.c

--- area_rect.c
+++ area_rect.c
@@ -3,18 +3,28 @@
 int main() {
              float len,wid;
              float area;
+            char ch;
+            
+          
+           do{
              printf("Enter the value of len: ");
              scanf("%f",&len);
-             printf("Enter the value of wid: ");
-             scanf("%f",&wid);
-             if(len>0 && wid>0)
+            printf("Enter the value of wid: ");
+            scanf("%f",&wid);
+            if(len>0 && wid>0)
              {
-             area = len*wid;
-             printf("Area of rectangle: %.2f",area);
-             }
-             else{
+            area = len*wid;
+            printf("Area of rectangle: %.2f",area);
+            }
+            else{
                  printf("Cannot calculate as negative values are given");
-             }
-             
+                }
+                printf("\nEnter your choice: ");
+            scanf(" %c",&ch);
+             } while (ch=='y');
+             if(ch!='y')
+             {
+             printf("Code finished");}
+           
     return 0;
 }
