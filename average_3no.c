f3e6c58e9a9a73344ff8697bfb2addbd23a365fa
Priyani Chourey <priyanichourey.dev@gmail.com>
2025-07-10 12:05:28 +0530

    Create average_3no.c

---
+++ average_3no.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+   int a,b,c,avg;
+   printf("Enter the value of a: ");
+   scanf("%d",&a);
+    printf("Enter the value of b: ");
+   scanf("%d",&b);
+    printf("Enter the value of c: ");
+   scanf("%d",&c);
+   avg=(a+b+c)/2;
+   printf("Average of %d,%d,%d is %d",a,b,c,avg);
+   
+   
+    return 0;
+}
