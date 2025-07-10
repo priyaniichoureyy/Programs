94d25c72f6684bd6a330021357f2b313b4732450
Priyani Chourey <priyanichourey.dev@gmail.com>
2025-07-10 12:01:25 +0530

    Update volume_spher.c

--- volume_spher.c
+++ volume_spher.c
@@ -2,11 +2,11 @@
 
 int main() {
    int volume,rad;
-   int pie=3.14;
+   int pie=22/7;
    printf("Enter the radius of sphere: ");
    scanf("%d",&rad);
    volume=pie*(rad*rad*rad);
-   volume=volume*(4/3);
+   volume=(volume*4)/3;
    printf("%d is the volume of sphere",volume);
     return 0;
 }
